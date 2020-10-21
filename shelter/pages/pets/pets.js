774743208e9a33f6b8eba3fad91a67beb0a546d6
Anna ZAS <ansiv@yandex.ru>
2020-10-21 12:45:27 +0300

    feat: add modal window to pets page

---
+++ shelter/pages/pets/pets.js
@@ -0,0 +1,122 @@
+const body = document.querySelector('body');
+const menuMobileBtn = document.querySelector('.menu-mobile__btn');
+const mainNav = document.querySelector('.main-nav');
+const cardsPets = document.querySelectorAll('.cards__item');
+const modal = document.querySelector('.modal');
+
+let petsData = [];
+
+const getData = async function(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Error at address ${url},
+        status ${response.status}!`);
+  }
+  return await response.json();
+};
+
+function init() {
+  getData('../../assets/pets.json').then(function(data) {
+    //console.log(data);
+    petsData.push(...data);
+    //console.log(petsData);
+  });
+  //console.log(petsData[0]);
+  //console.log(sliderData);
+  //return petsData;
+};
+
+
+//const currentSlide = sliderData.push(randomGen(3, petsData));
+//console.log(currentSlide);
+//console.log(randomGen(2, [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]))
+//! menu-burger
+menuMobileBtn.addEventListener('click', () => {
+  menuMobileBtn.classList.toggle('menu-mobile__btn--active')
+});
+
+//* open modal window
+function openModal(event) {
+  const cardsPets = event.target.closest('.cards__item');
+  const petName = cardsPets.querySelector('.cards__item__name').textContent;
+  const modalImg = modal.querySelector('.modal__img');
+  const modalName = modal.querySelector('.modal__description__name');
+  const modalBreed = modal.querySelector('.modal__description__breed');
+  const modalContent = modal.querySelector('.modal__description__content');
+  const modalAge = modal.querySelector('[data-age]');
+  const modalInoculations = modal.querySelector('[data-inoculations]');
+  const modalDiseases = modal.querySelector('[data-diseases]');
+  const modalParasites = modal.querySelector('[data-parasites]');
+
+  const petInfo = petsData.filter((item) => {
+    if (item.name === petName) return item;
+  });
+
+  const { age, breed, description, diseases, img, inoculations, name, parasites, type } = petInfo[0];
+
+  modalImg.setAttribute('src', '');
+  modalName.textContent = '';
+  modalBreed.textContent = '';
+  modalContent.textContent = '';
+  modalAge.textContent = '';
+  modalInoculations.textContent = '';
+  modalDiseases.textContent = '';
+  modalParasites.textContent = '';
+
+  modal.classList.remove('hide');
+  body.classList.add('locked');
+
+  modalImg.setAttribute('src', img);
+  modalName.textContent = name;
+  modalBreed.textContent = `${type} - ${breed}`;
+  modalContent.textContent = description;
+  modalAge.textContent = age;
+  modalInoculations.textContent = inoculations;
+  modalDiseases.textContent = diseases;
+  modalParasites.textContent = parasites;
+};
+
+// pagination
+//* функция, которая генерит заданное кол-во случайных элементов из массива, который подается на вход
+function randomGen(quantity = 1, array = []) {
+  if (array.length === 0) return array;
+
+  const unique = new Set();
+
+  while (unique.size < quantity) {
+    let indexRandom = Math.round(Math.random() * (array.length - 1));
+    unique.add(array[indexRandom]);
+  }
+  const randomArray = Array.from(unique);
+  return randomArray;
+};
+
+// function slideChange(event) {
+//   const sliderCards = slider.querySelectorAll('.slider__card');
+//   const randomSlide = randomGen(8, petsData);
+
+//   sliderCards.forEach((item, index) => {
+//     item.querySelector('.slider__card__img').setAttribute('src', randomSlide[index].img);
+//     item.querySelector('.slider__card__name').textContent = randomSlide[index].name;
+//   });
+// }
+
+
+//* event listeners
+cardsPets.forEach((item) => {
+  item.addEventListener('click', openModal);
+});
+
+// sliderButtons.forEach((item) => {
+//   item.addEventListener('click', slideChange)
+// })
+
+modal.addEventListener('click', (event) => {
+  console.log(event.target)
+  if (event.target.closest('.btn--secondary--arrow--cross') || event.target.classList.contains('modal')) {
+    body.classList.remove('locked');
+    modal.classList.add('hide');
+  }
+});
+
+init();
