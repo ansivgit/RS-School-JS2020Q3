3c6fb5836251e914a9440989960ae6e0e8502e97
Anna ZAS <ansiv@yandex.ru>
2020-10-23 21:00:19 +0300

    add: display weather

--- momentum/script.js
+++ momentum/script.js
@@ -1,3 +1,5 @@
+const API_KEY = '0abbbbc44095a2020989c4f5c972b547';
+
 const imagesAll = {
   night: ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'],
   morning: ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'],
@@ -15,17 +17,16 @@ const time = document.querySelector('.time');
 const day = document.querySelector('.day');
 const greeting = document.querySelector('.greeting');
 const name = document.querySelector('.name');
-const focus = document.querySelector('.focus');
+const myFocus = document.querySelector('.my-focus');
 const changeImgBtn = document.querySelector('.btn--bgImg-change');
-const blockquote = document.querySelector('blockquote');
-const figcaption = document.querySelector('figcaption');
-const btnQuoteChange = document.querySelector('.btn--quote-change');
+const changeQuoteBtn = document.querySelector('.btn--quote-change');
+const city = document.querySelector('.city');
 
 function showTime() {
   let today = new Date();
+  let second = today.getSeconds();
   hour = today.getHours();
   minute = today.getMinutes();
-  let second = today.getSeconds();
 
   time.textContent = `${hour}:${addZero(minute)}:${addZero(second)}`;
   if (parseInt(minute) + second === 0) {
@@ -74,7 +75,7 @@ function randomGen(quantity = 1, array = []) {
 
 function setBgGreetings() {
   let hour = new Date().getHours();
-  console.log('hola at ', new Date());
+  //console.log('hola at ', new Date());
 
   document.body.style.backgroundImage = `url('./assets/images/${dayImages[hour]}')`;
 
@@ -135,19 +136,19 @@ function clearField(event) {
 }
 
 function getFocus() {
-  focus.textContent = (localStorage.getItem('focus') === null)
+  myFocus.textContent = (localStorage.getItem('myFocus') === null)
     ? '[Enter Focus]'
-    : focus.textContent = localStorage.getItem('focus');
+    : myFocus.textContent = localStorage.getItem('myFocus');
 };
 
 function setFocus(event) {
   if (event.type === 'keydown') {
     if (event.key === 'Enter') {
-      localStorage.setItem('focus', event.target.textContent);
-      focus.blur();
+      localStorage.setItem('myFocus', event.target.textContent);
+      myFocus.blur();
     }
   } else {
-    localStorage.setItem('focus', event.target.textContent);
+    localStorage.setItem('myFocus', event.target.textContent);
   }
 };
 
@@ -160,7 +161,9 @@ function changeBgImage() {
 // префикс https://cors-anywhere.herokuapp.com используем для доступа к данным с других сайтов,
 //если браузер возвращает ошибку Cross - Origin Request Blocked
 async function getQuote() {
-  console.log('0000');
+  const blockquote = document.querySelector('blockquote');
+  const figcaption = document.querySelector('figcaption');
+
   //префикс https://cors-anywhere.herokuapp.com//
   const url = `https://favqs.com/api/qotd`;
   //const url = `https://https://type.fit/api/quotes`;
@@ -172,23 +175,57 @@ async function getQuote() {
 
   const res = await fetch(url);
   const data = await res.json();
-  console.log(data);
-  blockquote.textContent = `"${data.quote.body}"`;
+
+  blockquote.textContent = `«${data.quote.body}»`;
   figcaption.textContent = data.quote.author;
-  console.log('1111');
-}
+};
+
+async function getWeather() {
+  const weatherIcon = document.querySelector('.weather-icon');
+  const weather = document.querySelector('.weather');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=${API_KEY}&units=metric`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  weatherIcon.className = 'weather-icon owf';
+
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  weather.textContent = `${data.main.temp}°C, ${data.main.humidity}%, ${data.wind.speed}m/sec`;
+};
+
+function getCity() {
+  city.textContent = (localStorage.getItem('city') === null)
+    ? '[Enter City]'
+    : city.textContent = localStorage.getItem('city');
+};
+
+function setCity(event) {
+  if (event.type === 'keydown') {
+    if (event.key === 'Enter') {
+      localStorage.setItem('city', event.target.textContent);
+      city.blur();
+    }
+  } else {
+    localStorage.setItem('city', event.target.textContent);
+  }
+};
 
 //name.addEventListener('click', clearField);
 name.addEventListener('keydown', setName);
 name.addEventListener('blur', setName);
-focus.addEventListener('keydown', setFocus);
-focus.addEventListener('blur', setFocus);
+myFocus.addEventListener('keydown', setFocus);
+myFocus.addEventListener('blur', setFocus);
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keydown', setCity);
+city.addEventListener('blur', setCity);
 changeImgBtn.addEventListener('click', changeBgImage);
 document.addEventListener('DOMContentLoaded', getQuote);
-btnQuoteChange.addEventListener('click', getQuote);
+changeQuoteBtn.addEventListener('click', getQuote);
 
 showTime();
 showDate();
 setBgGreetings();
 getName();
 getFocus();
+getCity();
