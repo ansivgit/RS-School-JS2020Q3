7c3c28507ea8ccdd7898a1965063d7fb77525ae8
Anna ZAS <ansiv@yandex.ru>
2020-11-24 17:22:54 +0300

    fix: add clear lines

--- gem-puzzle/source/images.js
+++ gem-puzzle/source/images.js
@@ -1,8 +1,11 @@
 export function getBgPathes(path) {
   const imgArr = [];
+
   for (let i = 1; i <= 10; i += 1) {
     const imgPath = `${path}vl_00${i}`;
+
     imgArr.push(imgPath);
   }
+
   return imgArr;
-}
+};
